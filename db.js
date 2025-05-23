ebf1e68da1c47e6a2ba745f5bd80729cfef95f7e
Mudit jain <muditj214@gmail.com>
2025-05-23 15:36:43 +0530

    re-deploying

--- db.js
+++ db.js
@@ -1,23 +1,24 @@
-const mysql = require("mysql2");
-require("dotenv").config();
+const mysql = require('mysql2');
+const fs = require('fs');
+require('dotenv').config();
 
-console.log("Connecting to MySQL with: ", {
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync('./aiven-ca.pem'),  // place this file in your project root
+  },
 });
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+connection.connect(err => {
+  if (err) {
+    console.error('MySQL connection error:', err);
+    process.exit(1);
+  }
+  console.log('Connected to Aiven MySQL database with SSL');
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log("MySQL connected!");
-});
-
-module.exports = db;
+module.exports = connection;
